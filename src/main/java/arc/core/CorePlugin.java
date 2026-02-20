49f32b57d676e2f21813ae21bb5b27d62da2ff75
elementarc <arctale.void@gmail.com>
2026-02-20 15:29:21 +0100

    Hotfixes and added manifest

---
+++ src/main/java/arc/core/CorePlugin.java
@@ -0,0 +1,26 @@
+package arc.core;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class CorePlugin extends JavaPlugin {
+
+  public CorePlugin(@Nonnull JavaPluginInit init) {
+    super(init);
+    
+  }
+
+  @Override
+  protected void setup() {
+    super.setup();
+   
+  }
+
+  @Override
+  protected void start() {
+    
+    return;
+  }
+}
